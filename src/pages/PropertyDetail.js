b81bf8c898cb4b8669adebb0a6ff4947ede34b39
Avinash Maurya <avmaurya07@gmail.com>
2026-01-19 17:43:18 +0530

    react-markdown-removed

--- src/pages/PropertyDetail.js
+++ src/pages/PropertyDetail.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import ReactMarkdown from "react-markdown";
 
 function PropertyDetail() {
   const { id } = useParams();
